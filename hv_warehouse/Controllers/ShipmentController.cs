fc95140d84ab423f17bddcbb550c1069658d68e1
Dmytro Petrenko <dmytro.petrenko@sigma.software>
2020-05-30 10:29:20 +0300

    added shipments controller

---
+++ hv_warehouse/Controllers/ShipmentController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using hv_warehouse.Models;
+using System.Linq;
+
+namespace hv_warehouse.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShipmentController : ControllerBase
+    {
+        private readonly warehouse_dbContext _context;
+
+        public ShipmentController(warehouse_dbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllShipments()
+        {
+            string sql = "SELECT * FROM shipments";
+
+            var shipmentList = await _context.Shipments.FromSqlRaw(sql)
+                .ToListAsync();
+
+            return Ok(shipmentList);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetShipment(int id)
+        {
+            string sql = "SELECT * FROM shipments WHERE shipment_id = {0}";
+            var shipment = await _context.Shipments.FromSqlRaw(sql, id).FirstOrDefaultAsync();
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+            return Ok(shipment);
+        }
+
+        [HttpPost("new")]
+        public async Task<IActionResult> AddShipment(
+            [FromQuery] string partId,
+            [FromQuery] int shipmentQty,
+            [FromQuery] DateTime shipmentDate,
+            [FromQuery] int customerId
+            )
+        {
+            string sqlDate = shipmentDate.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            var result = await _context.Database.ExecuteSqlInterpolatedAsync($"CALL shipment_insert({partId}, {shipmentQty}, {sqlDate}, {customerId})");
+            if (result == -1)
+            {
+                return Ok("Added successfully");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> UpdateShipment(
+                    [FromRoute] int id,
+                    [FromQuery] string partId,
+                    [FromQuery] int shipmentQty,
+                    [FromQuery] DateTime shipmentDate
+                    )
+        {
+            string sql = "SELECT * FROM shipments WHERE shipment_id = {0}";
+            string sqlDate = shipmentDate.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+            var shipment = await _context.Shipments.FindAsync(id);
+            if (shipment == null || shipment.ShipmentQty == shipmentQty)
+                return NotFound();
+
+            if (shipment.ShipmentQty > shipmentQty)
+            {
+                var number = shipment.ShipmentQty - shipmentQty;
+                var query1 = await _context.Database.ExecuteSqlInterpolatedAsync($"CALL shipment_update({id}, {partId}, {shipmentQty}, {sqlDate})");
+                if (query1 == -1)
+                {
+                    await _context.Database.ExecuteSqlInterpolatedAsync($"CALL warehouse_after_shipment_update({partId}, {-number})");
+                    var result1 = await _context.Shipments.FromSqlRaw(sql, id).FirstOrDefaultAsync();
+                    return Ok(result1);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            var partNumber = shipmentQty - shipment.ShipmentQty;
+            var query2 = await _context.Database.ExecuteSqlInterpolatedAsync($"CALL shipment_update({id}, {partId}, {shipmentQty}, {sqlDate})");
+            if (query2 != -1)
+            {
+                return NotFound();
+            }
+            await _context.Database.ExecuteSqlInterpolatedAsync($"CALL warehouse_after_shipment_update({partId}, {partNumber})");
+            var result2 = await _context.Shipments.FromSqlRaw(sql, id).FirstOrDefaultAsync();
+            return Ok(result2);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShipment(
+                    [FromRoute] int id
+                    )
+        {
+            var shipment = await _context.Shipments.FindAsync(id);
+            if (shipment == null)
+                return NotFound();
+
+            var deletedRows = await _context.Database.ExecuteSqlInterpolatedAsync($"CALL shipment_delete({id})");
+            await _context.Database.ExecuteSqlInterpolatedAsync($"CALL warehouse_after_shipment_update({shipment.PartId}, {-shipment.ShipmentQty})");
+            return Ok(deletedRows);
+        }
+    }
+}
--- hv_warehouse/Controllers/WarehouseController.cs
+++ hv_warehouse/Controllers/WarehouseController.cs
@@ -22,8 +22,6 @@ namespace hv_warehouse.Controllers
         {
             string sql = "SELECT * FROM warehouses";
 
-            //string sql1 = "SELECT w.warehouse_id, p.part_name, w.part_qty, w.warehouse_address, w.warehouse_location FROM warehouses w JOIN parts p ON w.part_id = p.part_id;";
-
             var warehouseList = await _context.Warehouses.FromSqlRaw(sql).ToListAsync();
             return Ok(warehouseList);
         }
@@ -39,32 +37,5 @@ namespace hv_warehouse.Controllers
             }
             return Ok(warehouse);
         }
-
-
-        /*[HttpGet("old")]
-        public async Task<IActionResult> GetAsync()
-        {
-            List<Warehouse> warehouseList = new List<Warehouse>();
-            var connectionString = _configuration.GetConnectionString("WarehouseDB");
-
-            await using var conn = new NpgsqlConnection(connectionString);
-            await conn.OpenAsync();
-
-            string sql = "SELECT * FROM warehouses";
-
-            await using (var cmd = new NpgsqlCommand(sql, conn))
-            await using (var reader = await cmd.ExecuteReaderAsync())
-                while (await reader.ReadAsync())
-                {
-                    var warehouse = new Warehouse();
-                    warehouse.Warehouse_id = reader["warehouse_id"].ToString();
-                    warehouse.Part_id = reader["part_id"].ToString();
-                    warehouse.Part_qty = Convert.ToInt32(reader["part_qty"]);
-                    warehouse.Warehouse_address = reader["warehouse_address"].ToString();
-                    warehouse.Warehouse_location = reader["warehouse_location"].ToString();
-                    warehouseList.Add(warehouse);
-                }
-            return Ok(warehouseList);
-        }*/
     }
 }
